d4be1045ffde30998eaba8bc53310d1049213533
David Holstius <david.holstius@gmail.com>
2019-11-15 13:51:17 -0800

    archive older chart_annual_quantities()

--- R/Archive/chart_annual_quantities.R
+++ R/Archive/chart_annual_quantities.R
@@ -1,94 +1,52 @@
 #' chart_annual_quantities
 #'
-#' @param data tabular data with column `year`; passed to `ggplot()`
-#' @param mapping passed to `ggplot()`
-#' @param year_limits four-digit integers; passed to [scale_x_annual()]
-#' @param year_breaks four-digit integers; passed to [scale_x_annual()]
-#' @param year_expand passed to [scale_x_annual()]
-#' @param ... reserved for future use
+#' @note foundation for chart_annual_*
 #'
 #' @seealso
-#' - [chart_annual_emissions()]
 #' - [chart_annual_growth()]
+#' - [chart_annual_emissions()]
 #'
 #' @export
 chart_annual_quantities <- function (
-  data = NULL,
-  mapping = aes(),
+  chart_data,
   year_limits = c(NA, NA),
-  year_breaks = seq(1990, 2050, by = 5),
-  year_expand = expand_scale(add = c(0, 1), mult = c(0, 0)),
-  title = NULL,
-  subtitle = NULL,
-  caption = NULL,
-  ...,
-  verbose = getOption("verbose")
+  year_breaks = seq(1990, 2050, by = 10),
+  year_expand = expand_scale(add = c(1, 1), mult = c(0, 0)),
+  ...
 ) {
 
   require(yeartools)
 
   chart_color_scale <-
-    ggthemes::scale_color_few(
-      palette = "Dark")
+    scale_color_excel_new()
 
   chart_fill_scale <-
-    ggthemes::scale_fill_few(
-      palette = "Dark")
+    scale_fill_excel_new()
 
   chart_theme <-
     theme_simple() +
     theme(
-      plot.subtitle = element_text(size = rel(0.9)))
+      plot.subtitle = element_text(
+        size = rel(0.9)))
 
   if (!all_true(is.na(year_limits))) {
     year_limits <- elide_year(year_limits)
   }
 
-  if (is.null(data)) {
-
-    chart_x_scale <-
-      scale_x_annual(
-        limits = year_limits,
-        expand = year_expand)
-
-  } else {
-
-    chart_x_scale <-
-      scale_x_annual(
-        limits = year_limits,
-        breaks = year_breaks,
-        expand = year_expand)
-
-  }
-
-  chart_description <- local({
-
-    # Default chart caption: "DRAFT YYYY-mm-dd".
-    if (is.null(caption)) {
-      caption <-
-        glue::glue(
-          'DRAFT {strtools::str_datestamp()}',
-          .sep = "\n")
-    }
-
-    chart_description <-
-      labs(
-        title = title,
-        subtitle = subtitle,
-        caption = caption)
-
-  })
+  chart_x_scale <-
+    scale_x_annual(
+      limits = year_limits,
+      breaks = elide_year(year_breaks),
+      expand = year_expand)
 
   chart_object <-
-    ggplot(
-      data = data,
-      mapping = mapping) +
+    chart_data %>%
+    ggplot() +
     aes(x = yeartools::elide_year(year)) +
     chart_theme +
     chart_x_scale +
     chart_color_scale +
-    chart_fill_scale +
-    chart_description
+    chart_fill_scale
 
   return(chart_object)
 
