67d4a86fe26d97bdfa74c62034e275dc1c824aaa
David Holstius <david.holstius@gmail.com>
2019-11-15 09:00:17 -0800

    allow digits argument

--- R/scale_y_percentage.R
+++ R/scale_y_percentage.R
@@ -1,4 +1,13 @@
 #' @export
-scale_y_percentage <- function (..., limits = c(0, 1), breaks = seq(0, 1, by = 0.2), expand = c(0, 0)) {
-  scale_y_continuous(..., labels = format_percentage, limits = limits, breaks = breaks, expand = expand)
+scale_y_percentage <- function (
+  ...,
+  digits = 0,
+  expand = c(0, 0)
+) {
+
+  scale_y_continuous(
+    ...,
+    labels = partial(format_percentage, digits = digits),
+    expand = expand)
+
 }
