b2733b823dfafdb794d83961b1626a6951b68a77
David Holstius <david.holstius@gmail.com>
2020-02-20 11:13:39 -0800

    support reversing of percentage scale

--- R/scale_y_percentage.R
+++ R/scale_y_percentage.R
@@ -4,12 +4,23 @@
 scale_y_percentage <- function (
   ...,
   digits = 0,
-  expand = c(0, 0)
+  expand = c(0, 0),
+  reverse = FALSE
 ) {
+
+  if (isTRUE(reverse)) {
+    label_fun <- function (x) {
+      format_percentage(1 - x, digits = digits)
+    }
+  } else {
+    label_fun <- function (x) {
+      format_percentage(x, digits = digits)
+    }
+  }
 
   scale_y_continuous(
     ...,
-    labels = partial(format_percentage, digits = digits),
+    labels = label_fun,
     expand = expand)
 
 }
