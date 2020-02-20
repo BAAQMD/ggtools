a9709ca7bdaed20d46c662e53c5f1faa9a0b7199
David Holstius <david.holstius@gmail.com>
2020-02-20 08:34:04 -0800

    revert from manual to auto breaks on y scale

--- R/chart_annual_growth_by.R
+++ R/chart_annual_growth_by.R
@@ -132,7 +132,7 @@ chart_annual_growth_by <- function (
       scale_y_percentage(
         NULL, # #glue::glue("{qty_var} (normalized to {base_year})"),
         limits = c(0, NA),
-        breaks = seq(0, 10, by = 0.2),
+        #breaks = seq(0, 10, by = 0.2),
         expand = expand_scale(mult = c(0, 0.3)),
         sec.axis = dup_axis())
 
