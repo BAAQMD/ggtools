89be6ad7fbc513b2c94639f619cc10adf8e32cde
David Holstius <david.holstius@gmail.com>
2020-03-18 09:42:22 -0700

    migrate expand_scale() to expansion()

--- tests/testthat/test-chart_annual_throughputs_by.R
+++ tests/testthat/test-chart_annual_throughputs_by.R
@@ -2,14 +2,16 @@ context("chart_annual_throughputs")
 
 test_that("chart_annual_throughputs_by, single pollutant", {
 
-  test_tput_data %>%
-    filter(
-      pol_abbr == "NOx") %>%
-    chart_annual_throughputs_by(
-      cat_id,
-      flag_years = CY(2011),
-      base_year = CY(2011),
-      verbose = TRUE)
+  expect_warning(
+    test_tput_data %>%
+      filter(
+        pol_abbr == "NOx") %>%
+      chart_annual_throughputs_by(
+        cat_id,
+        flag_years = CY(2011),
+        base_year = CY(2011),
+        verbose = TRUE),
+    "pol_abbr, ems_qty, ems_unit")
 
 })
 
