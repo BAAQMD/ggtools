3fd2c2d63d857b9dcfa5357de2c30b81e8433d60
David Holstius <david.holstius@gmail.com>
2021-02-16 14:06:12 -0800

    rewrite scale_x_log10() for better breaks

--- R/scale_x_log10.R
+++ R/scale_x_log10.R
@@ -1,4 +1,4 @@
-#' log10 scale together with axis ticks
+#' log10 scale together with axis ticks and breaks
 #'
 #' @param ... passed to [ggplot2::scale_x_log10()]
 #' @param labels function
@@ -8,12 +8,32 @@
 #' @export
 scale_x_log10 <- function (
   ...,
-  labels = format_log10
+  limits,
+  labels = ggtools::label_log10,
+  expand = expansion(mult = c(0.01, 0.01)),
+  verbose = getOption("verbose", default = FALSE)
 ) {
+
+  msg <- function (...) if(isTRUE(verbose)) message("[scale_x_log10] ", ...)
+
+  # if (is.null(labels)) {
+  #   labels <- function (x) label_log10(x)
+  # }
+
+  log10_breaks <- seq(
+    floor(min(log10(limits))),
+    ceiling(max(log10(limits))),
+    by = 1)
+
+  minor_breaks <-
+    as.numeric(c(1:9) %o% (10 ^ log10_breaks))
 
   scale_object <-
     ggplot2::scale_x_log10(
       ...,
+      limits = limits,
+      breaks = 10 ^ log10_breaks,
+      minor_breaks = minor_breaks,
       labels = labels)
 
   annotation_object <-
