18eeaccb58d407d78af6e2c45e6f7770a05992b4
David Holstius <david.holstius@gmail.com>
2021-02-16 14:06:25 -0800

    Create label_log10.R

---
+++ R/label_log10.R
@@ -0,0 +1,38 @@
+#' label_log10
+#'
+#' @param x
+#' @param TeX
+#'
+#' @importFrom latex2exp TeX
+#' @importFrom stringr str_c
+#'
+#' @return
+#' @export
+#'
+label_log10 <- function (x, digits = 1, TeX = TRUE, output = "expression") {
+
+  str_replace_expr <- function (expr, pattern, replacement) {
+    deparsed <- purrr::map(expr, deparse)
+    f <- function (x) str_replace_all(x, pattern, replacement)
+    replaced <- purrr::map(deparsed, f)
+    collapsed <- purrr::map(replaced, str_c, collapse = "")
+    formalized <- map(collapsed, str2lang) # turn strings back into calls
+    return(as.expression(formalized))
+  }
+
+  if (isTRUE(TeX)) {
+
+    formatted <- stringr::str_c("10^{", round(log10(x), digits = digits), "}")
+    sanitized <- tidyr::replace_na(formatted, "")
+    latex_expr <- latex2exp::TeX(sanitized, output = output)
+    labels <- str_replace_expr(latex_expr, "\\.\\s+([0-9]+)", ".\\1")
+
+  } else {
+
+    labels <- stringr::str_c("10^", log10(x))
+
+  }
+
+  return(labels)
+
+}
