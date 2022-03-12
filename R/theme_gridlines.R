9ec3c40278703c73747e6146d1bb0a6ead64e946
David Holstius <david.holstius@gmail.com>
2022-03-11 16:43:54 -0800

    add theme_gridlines()

--- R/theme_gridlines.R
+++ R/theme_gridlines.R
@@ -1,6 +1,27 @@
-theme_gridlines <- function (linetype = "dotted", color = gray(0.9), ...) {
-  line_element <- element_line(linetype = linetype, color = color, ...)
-  theme(
-    panel.grid.major.x = line_element,
-    panel.grid.major.y = line_element)
+#' theme_gridlines
+#'
+#' Subtle gridlines for a ggplot object.
+#' Useful in conjunction with [theme_simple()].
+#'
+#' @param linetype (character) passed to [ggplot2::element_line()]
+#' @param color (character) passed to [ggplot2::element_line()]
+#' @param ... further arguments to [ggplot2::element_line()]
+#'
+#' @export
+theme_gridlines <- function (
+  linetype = "dotted",
+  color = gray(0.9),
+  ...
+) {
+
+  line_element <-
+    ggplot2::element_line(
+      linetype = linetype,
+      color = color,
+      ...)
+
+  theme_object <-
+    ggplot2::theme(
+      panel.grid.major.x = line_element,
+      panel.grid.major.y = line_element)
 }
