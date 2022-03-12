6761acb263af14a13641629e46b4ee48788a6ab2
David Holstius <david.holstius@gmail.com>
2022-03-11 16:41:30 -0800

    add theme_remove()

--- R/theme_remove.R
+++ R/theme_remove.R
@@ -1,5 +1,30 @@
+#' theme_remove
+#'
+#' Removes specified theme elements by setting them to [ggplot2::theme_blank()].
+#'
+#' @details
+#' All combinations of the elements in `...` will be removed.
+#' See examples.
+#'
+#' @param ... (character) see Details and examples
+#'
+#' @importFrom purrr cross map_chr
+#' @importFrom stringr str_c
+#' @importFrom ggplot2 element_blank theme
+#' @importFrom rlang exec set_names
+#'
+#' @export
+#'
+#' @examples
+#' p <- ggplot(mtcars) + geom_point()
+#' p + theme_remove("axis", "text")
+#' p + theme_remove("axis", c("text", "line"), "x")
+#' p + theme_remove("axis", c("text", "line"), c("x", "y"))
 theme_remove <- function (...) {
-  element_names <- map_chr(purrr::cross(list(...)), str_c, collapse = ".")
-  element_objects <- replicate(length(element_names), element_blank())
-  rlang::exec(ggplot2::theme, !!!set_names(element_objects, element_names))
+  crossed <- purrr::cross(list(...))
+  element_names <- purrr::map_chr(crossed, stringr::str_c, collapse = ".")
+  element_objects <- replicate(length(element_names), ggplot2::element_blank())
+  named_element_objects <- rlang::set_names(element_objects, element_names)
+  theme_object <- rlang::exec(ggplot2::theme, !!!named_element_objects)
+  return(theme_object)
 }
